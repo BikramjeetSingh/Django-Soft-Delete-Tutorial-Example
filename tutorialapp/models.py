be6ea70432b19caa6eedfd8f637301a4a7049fa8
BikramjeetSingh <singhbikramjeet@outlook.com>
2021-06-26 10:25:24 +0530

    Store deleted_at datetime

--- tutorialapp/models.py
+++ tutorialapp/models.py
@@ -1,25 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class SoftDeleteManager(models.Manager):
 
     def get_queryset(self):
-        return super().get_queryset().filter(is_deleted=False)
+        return super().get_queryset().filter(deleted_at__isnull=True)
 
 
 class SoftDeleteModel(models.Model):
 
-    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, default=None)
     objects = SoftDeleteManager()
     all_objects = models.Manager()
 
     def soft_delete(self):
-        self.is_deleted = True
+        self.deleted_at = timezone.now()
         self.save()
 
     def restore(self):
-        self.is_deleted = False
+        self.deleted_at = None
         self.save()
 
     class Meta:
